db169666b9ea76cdfaf408dfdf261840c90c77d1
austinburnett <aburn1011@gmail.com>
2023-03-22 12:34:24 -0400

    [Feat] Init MongoDB with test data

--- Server/src/docker-entrypoint-initdb.d/mongoInit.js
+++ Server/src/docker-entrypoint-initdb.d/mongoInit.js
@@ -1,5 +1,12 @@
 print("################################ INITIALIZING DB INFO ################################");
+print("################################ #################### ################################");
 
-//db.adminCommand('listDatabases');
+//db = db.getSiblingDB("instaCloneTest");
+//db.createCollection("users");
+db.users.insertOne({
+  email: "test@gmail.com",
+  password: "$argon2id$v=19$m=4096,t=3,p=1$RTMqhq724f7WNkM3wMtoxg$3j9ceDcd+9k2xAKh8B/iyUlk5KpE4Wak4PRLyMvnDyQ",
+});
 
-print("################################ END INIT DB INFO ################################");
+print("################################ END INIT DB INFO ####################################");
+print("################################ ################ ####################################");
