bcc4be2a4f00d92958fefb9a83b94c613a24cbfb
austinburnett <aburn1011@gmail.com>
2023-04-11 13:54:49 -0400

    Migrate express app to aws lambda

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,35 +1,9 @@
 const express = require("express");
-const dotenv = require("dotenv");
 const routes = require("./routes/routes");
-const mongoUtil = require("./util/mongoUtil");
+const serverless = require("serverless-http");
 const app = express();
 
-// Setup .env
-dotenv.config();
-
-// Listen on specified port
-app.listen(process.env.PORT, () => {
-    console.log(`Listening on port ${ process.env.PORT }`);
-});
-
-// Template Engine Setup
-app.set("views", "./views");
-
 // Initial route
-app.use("/", routes);
+app.use("/expressApp", routes);
 
-// Connect to db
-mongoUtil.connect(() => {});
-
-// Close all connections when the application recieves SIGINT signal
-process.on("SIGINT", async() => {
-    db = mongoUtil.getDb();
-    try{
-        await db.disconnect();
-    } catch(err){
-        console.error(err);
-    } finally{
-        console.log("\nMongoDB connections closed.");
-        process.exit();
-    }
-});
+module.exports.handler = serverless(app);
