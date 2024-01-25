e3ffb2f6b8bc3872321f451c047e3f0df665a617
austinburnett <aburn1011@gmail.com>
2024-01-25 18:12:12 -0500

    [Refactor] Move like button logic out of post component

--- Client/src/pages/shared/post/Post.js
+++ Client/src/pages/shared/post/Post.js
@@ -1,27 +1,11 @@
 import React, { useState } from "react";
 import PostLikeButton from "./PostLikeButton.js";
 import PostCommentButton from "./PostCommentButton.js";
-import { useNavigate } from "react-router-dom";
 import { useId } from "react";
-import { jwtDecode } from "jwt-decode";
-import axios from "axios";
 import "./post.css";
-import createRequestConfig from "./createRequestConfig.js";
 
 const Post = ({postId, postUser, postImage, postCaption, postLikes, postComments}) => {
-    const [numLikes, setNumLikes] = useState(postLikes.length);
-    const [color, setColor] = useState("white");
-    const [isLiked, setIsLiked] = useState(null);
     const uid = useId();
-
-    let decodedToken = localStorage.jwt;
-    try{
-        decodedToken = jwtDecode(decodedToken);
-    } catch(err){
-        console.log(err);
-    }
-
-    const request = createRequestConfig("patch", `posts/${postId}/like`);
 
     let commentsList = postComments.map(postComments => 
         <li key={postComments._id} >
@@ -31,39 +15,18 @@ const Post = ({postId, postUser, postImage, postCaption, postLikes, postComments
         </li>
     );
 
-    if(isLiked == null){
-        postLikes.forEach(like => {
-            if(like.username == decodedToken.username){
-                setColor("red");
-                setIsLiked(true);
-            }
-        });
-    }
+    let usersLiked = []
+    postLikes.forEach(like => {
+        usersLiked.push(like.username);
+    });
     
-    function handleClick(){
-        if(isLiked){
-            setColor("white");
-            setNumLikes(numLikes - 1);
-            setIsLiked(false);
-        }
-        else{
-            setColor("red");
-            setNumLikes(numLikes + 1);
-            setIsLiked(true);
-        }
-        axios.request(request).then((response) => {
-            console.log(response);
-        });
-    }
-
     return(
         <>
             <div className="post">
                 <p>{ postUser }</p>
                 <img src={ postImage } />
-                <PostLikeButton postLikes={ numLikes } likePost={ handleClick }
-                                buttonColor={ color }/>
-                <PostCommentButton postId={ postId } id={uid}/>
+                <PostLikeButton postId={ postId } likes={ usersLiked } />
+                <PostCommentButton postId={ postId } id={ uid }/>
                 <br />
                 <p>{ postCaption }</p>
                 <br />
@@ -73,5 +36,4 @@ const Post = ({postId, postUser, postImage, postCaption, postLikes, postComments
     );
 }
 
-// Register is entrypoint
 export default Post;
--- Client/src/pages/shared/post/PostLikeButton.js
+++ Client/src/pages/shared/post/PostLikeButton.js
@@ -1,14 +1,55 @@
 import React, { useState } from "react";
+import axios from "axios";
+import createRequestConfig from "./createRequestConfig.js";
+import { jwtDecode } from "jwt-decode";
+import { useNavigate } from "react-router-dom";
 
-const PostLikeButton = ({postLikes, likePost, buttonColor}) =>{
+const PostLikeButton = ({postId, likes}) =>{
+    const [numLikes, setNumLikes] = useState(likes.length);
+    const [color, setColor] = useState("white");
+    const [isLiked, setIsLiked] = useState(null);
 
     let style = {
-        backgroundColor: buttonColor,
+        backgroundColor: color,
         padding: "10px"
     }
 
+    let decodedToken = localStorage.jwt;
+    try{
+        decodedToken = jwtDecode(decodedToken);
+    } catch(err){
+        console.log(err);
+    }
+
+    if(isLiked == null){
+        likes.forEach(like => {
+            if(like == decodedToken.username){
+                setColor("red");
+                setIsLiked(true);
+            }
+        });
+    }
+
+    function handleClick(){
+        const request = createRequestConfig("patch", `posts/${postId}/like`);
+
+        if(isLiked){
+            setColor("white");
+            setNumLikes(numLikes - 1);
+            setIsLiked(false);
+        }
+        else{
+            setColor("red");
+            setNumLikes(numLikes + 1);
+            setIsLiked(true);
+        }
+        axios.request(request).then((response) => {
+            console.log(response);
+        });
+    }
+
     return(
-        <button style={ style } onClick={ likePost }>Like {postLikes}</button>
+        <button style={ style } onClick={ handleClick }>Like {numLikes}</button>
     );
 }
 
